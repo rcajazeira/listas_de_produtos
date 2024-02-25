93e33bac4de18d3e9d34f1674475bcef0c9d8076
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2024-02-25 17:45:16 -0300

    Atualização
    Atualização

--- projeto_node/app.js
+++ projeto_node/app.js
@@ -3,35 +3,26 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-// Rota de boas-vindas para a raiz do servidor
-app.get('/', (req, res) => {
-    res.send('Bem-vindo à página inicial');
-});
-
-// Endpoint para raspagem de produtos da Amazon
 app.get('/api/scrape', async (req, res) => {
     const keyword = req.query.keyword;
 
-    // Verificar se a palavra-chave foi fornecida
     if (!keyword) {
         return res.status(400).json({ error: 'Palavra-chave não fornecida' });
     }
 
     try {
-        // Buscar conteúdo da página de resultados de pesquisa da Amazon
         const response = await axios.get(`https://www.amazon.com/s?k=${keyword}`);
         const html = response.data;
         const $ = cheerio.load(html);
 
-        // Extrair detalhes dos produtos da primeira página de resultados
         const products = [];
 
         $('.s-result-item').each((index, element) => {
-            const title = $(element).find('.a-text-normal').text().trim();
-            const rating = $(element).find('.a-icon-star-small').text().trim();
-            const reviews = $(element).find('.a-size-small').text().trim();
+            const title = $(element).find('.a-text-normal').text();
+            const rating = $(element).find('.a-icon-star-small').text();
+            const reviews = $(element).find('.a-size-small').text();
             const imageUrl = $(element).find('.s-image').attr('src');
 
             products.push({
@@ -52,3 +43,4 @@ app.get('/api/scrape', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
